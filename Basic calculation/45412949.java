e5728cf6de84db0dae2de8c00381e83f301ddfaa
문지원(JiwonMoon) <mon99745@gmail.com>
2022-07-03 18:57:55 +0900

    Calculation

---
+++ Basic calculation/45412949.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int i = sc.nextInt();
+
+for(int j=1; j<=9; j++){
+		System.out.println(i +" * "+ j + " = " + i*j );
+		}
+
+	}
+}
+
